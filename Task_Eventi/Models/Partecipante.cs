bc998bed844a989867d65fc9207471512980e955
andreas ubertini <ubertiniandreas@gmail.com>
2024-03-22 19:56:44 +0100

    updt

---
+++ Task_Eventi/Models/Partecipante.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task_Eventi.Models;
+
+public partial class Partecipante
+{
+    public int PartecipanteId { get; set; }
+
+    public string Nome { get; set; } = null!;
+
+    public string Cognome { get; set; } = null!;
+
+    public string Contatto { get; set; } = null!;
+
+    public string CodFis { get; set; } = null!;
+
+    public int EventoRif { get; set; }
+
+    public virtual Evento EventoRifNavigation { get; set; } = null!;
+}
